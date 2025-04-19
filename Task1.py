95e166644dd23291ad3ac7bb465db8768d0e091a
abhishekdubey1990 <abhishekdubey1990@gmail.com>
2025-04-19 23:42:19 +0530

    Add files via upload
    Task 1 completed for TuteDude

---
+++ Task1.py
@@ -0,0 +1,7 @@
+a= float(input ('enter first number : '))
+b= float(input('enter second number : '))
+
+print('Addition : ' ,a+b)
+print('Substraction : ',a-b)
+print('Multiplication: ',a*b)
+print('Division : ', a/b)
